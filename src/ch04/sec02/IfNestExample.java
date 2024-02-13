83b0ebd1fa1ca647510fff316c5ce8e12c119455
jylee102 <lilydodo11@gmail.com>
2024-02-13 10:18:45 +0900

    if문

---
+++ src/ch04/sec02/IfNestExample.java
@@ -0,0 +1,25 @@
+package ch04.sec02;
+
+public class IfNestExample {
+    public static void main(String[] args) {
+        /*
+        랜덤한 숫자를 구하는 공식
+        start부터 n개의 정수를 랜덤한 숫자로 구한다.
+        (int) (Math.random() * n) + start;
+         */
+        int score = (int) (Math.random() * 20) + 81; // 81~100까지의 랜점한 숫자를 구한다.
+        System.out.println("랜덤한 점수: " + score);
+
+        String grade;
+
+        if (score >= 90) {
+            if (score >= 95) grade = "A+";
+            else grade = "A";
+        } else {
+            if (score >= 85) grade = "B+";
+            else grade = "B";
+        }
+
+        System.out.println("학점: " + grade);
+    }
+}
