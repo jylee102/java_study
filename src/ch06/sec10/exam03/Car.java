a0b0fe9c8b68f7e71d2256b2aa9e11c7d5a3036a
jylee102 <lilydodo11@gmail.com>
2024-02-16 17:53:52 +0900

    static 블록/메소드 주의점

---
+++ src/ch06/sec10/exam03/Car.java
@@ -0,0 +1,39 @@
+package ch06.sec10.exam03;
+
+public class Car {
+    // 인스턴스 필드
+    int speed;
+
+    // 정적 필드
+    static String color = "red";
+
+    // 인스턴스 메소드
+    void run() {
+        System.out.println(speed + "으로 달립니다.");
+        // System.out.println(color);
+    }
+
+    // 정적 메소드
+    // 정적 멤버들은 객체를 생성하지 않아도 클래스가 메모리로 로딩될 때 자동으로 실행됨
+    // 인스턴스 멤버들은 객체를 생성해야 사용할 수 있으므로
+    // static 메소드, static 블록에서는 반드시 객체를 생성하고 사용해야 함.
+    static void simulate() {
+        Car myCar = new Car(); //클래스 내부에서 자기 자신을 객체로 생성 가능
+
+        myCar.speed = 200;
+        // System.out.println(color);
+        System.out.println(myCar.speed + "으로 달립니다.");
+//        System.out.println(speed + "으로 달립니다.");
+
+        myCar.run();
+    }
+
+    public static void main(String[] args) {
+        // 정적 메소드 호출 가능 -> 객체를 생성하지 않고 호출 가능
+        simulate(); // Car.simulate(); 같은 Car 클래스에 있어서 클래스명 생략 가능
+
+        Car myCar = new Car();
+        myCar.speed = 60;
+        myCar.run();
+    }
+}
