777f0872b3b2c1e672db54952d4454561251fcc6
jylee102 <lilydodo11@gmail.com>
2024-02-20 11:35:25 +0900

    필드와 메소드의 접근제한자

---
+++ src/ch06/sec13/exam03/package2/C.java
@@ -0,0 +1,19 @@
+package ch06.sec13.exam03.package2;
+
+import ch06.sec13.exam03.package1.A;
+
+public class C {
+    public void method() {
+        A a = new A();
+
+        // 다른 클래스내에 있는 필드에 접근하기
+        a.field1 = 1;
+//        a.field2 = 1; // 같은 패키지에서만 접근가능 (default)
+//        a.field3 = 1; // 같은 클래스에서만 접근가능 (private)
+
+        // 다른 클래스내에 있는 메소드에 접근하기
+        a.method1();
+//        a.method2(); // 같은 패키지에서만 접근가능 (default)
+//        a.method3(); // 같은 클래스에서만 접근가능 (private)
+    }
+}
