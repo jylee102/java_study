b46f880806dad21621b16d88f9508fa7993163b9
jylee102 <lilydodo11@gmail.com>
2024-02-20 10:55:30 +0900

    클래스의 접근제한자

---
+++ src/ch06/sec13/exam01/package1/A.java
@@ -0,0 +1,5 @@
+package ch06.sec13.exam01.package1;
+
+class A { // default 접근제한자: 같은 패키지 내에서만 사용 가능
+
+}
---
+++ src/ch06/sec13/exam01/package2/C.java
@@ -0,0 +1,8 @@
+package ch06.sec13.exam01.package2;
+
+import ch06.sec13.exam01.package1.B;
+
+public class C {
+    //    A a; // default 접근제한자는 다른 패키지에서 사용 불가
+    B b; // public 접근제한자는 영향을 전혀 받지 않음
+}
