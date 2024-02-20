1dbdf15689376378d7676b2353803e6e77f3aac9
jylee102 <lilydodo11@gmail.com>
2024-02-20 10:44:01 +0900

    패키지

---
+++ src/ch06/sec12/hyundai/Car.java
@@ -0,0 +1,18 @@
+package ch06.sec12.hyundai;
+
+import ch06.sec12.hankook.SnowTire;
+import ch06.sec12.kumho.*;
+import ch06.sec12.kumho.project.*;
+
+public class Car {
+    Test t = new Test();
+
+    ch06.sec12.hankook.Tire tire1 = new ch06.sec12.hankook.Tire();
+    ch06.sec12.kumho.Tire tire2 = new ch06.sec12.kumho.Tire();
+
+    // 컨트롤 + 스페이스바: 자동완성 리스트
+
+    SnowTire tire3 = new SnowTire();
+    AllSeasonTire tire4 = new AllSeasonTire();
+    RainTire tire5 = new RainTire();
+}
