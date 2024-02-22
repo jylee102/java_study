1f026af6f490e82d8a8e41d0772e184acace1961
jylee102 <lilydodo11@gmail.com>
2024-02-22 15:41:01 +0900

    추상클래스

---
+++ src/ch07/sec10/exam01/PhoneExample.java
@@ -0,0 +1,13 @@
+package ch07.sec10.exam01;
+
+public class PhoneExample {
+    public static void main(String[] args) {
+        // abstract 클래스는 객체 생성 X
+//        Phone phone= new Phone();
+
+        SmartPhone smartPhone = new SmartPhone("길동");
+        smartPhone.turnOn(); // Phone의 메소드
+        smartPhone.internetSearch(); // SmartPhone의 메소드
+        smartPhone.turnOff(); // Phone의 메소드
+    }
+}
