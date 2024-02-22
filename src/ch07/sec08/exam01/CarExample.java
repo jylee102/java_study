4b9499877bdf7f789f4023e2c134e6c141a940f6
jylee102 <lilydodo11@gmail.com>
2024-02-22 12:07:59 +0900

    다형성 구현(필드 이용)

---
+++ src/ch07/sec08/exam01/CarExample.java
@@ -0,0 +1,17 @@
+package ch07.sec08.exam01;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        // 필드를 이용해 자동타입변환이 일어난다.
+        myCar.tire = new HankookTire(); // Tire tire = new HankookTire()
+        myCar.run(); // tire.roll(); -> 오버라이드 된 메소드가 있으면 자식클래스에 있는 메소드 사용
+
+        myCar.tire = new KumhoTire(); // Tire tire = new KumhoTire()
+        myCar.run(); // tire.roll(); -> 오버라이드 된 메소드가 있으면 자식클래스에 있는 메소드 사용
+
+        myCar.tire = new Tire(); // Tire tire = new Tire();
+        myCar.run();
+    }
+}
