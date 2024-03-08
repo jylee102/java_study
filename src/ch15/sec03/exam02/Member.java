61616ea1681f9b802975a6bc48f0d7d06ad1fb51
jylee102 <lilydodo11@gmail.com>
2024-03-08 14:14:17 +0900

    SET 컬렉션

---
+++ src/ch15/sec03/exam02/Member.java
@@ -0,0 +1,27 @@
+package ch15.sec03.exam02;
+
+public class Member {
+    public String name;
+    public int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        System.out.println(name.hashCode());
+        // name과 age의 값이 같으면 동일한 hashCode가 리턴됨
+        return name.hashCode() + age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // 객체의 데이터를 비교
+        // name과 age값이 같으면 true
+        if (obj instanceof Member target) { // Object obj = new Member();
+            return target.name.equals(name) && (target.age == age);
+        } else return false;
+    }
+}
