803da9d59501a1431290fbf03da42045702821d3
jylee102 <lilydodo11@gmail.com>
2024-04-08 11:56:42 +0900

    문자 입출력 스트림

---
+++ src/ch17/sec04/exam02/ReadExample.java
@@ -0,0 +1,39 @@
+package ch17.sec04.exam02;
+
+import java.io.FileReader;
+import java.io.Reader;
+
+public class ReadExample {
+    public static void main(String[] args) {
+
+        try {
+            Reader reader = null;
+
+            // 한 문자씩 읽기
+            reader = new FileReader("C:/Temp/test.txt");
+
+            while (true) {
+                int data = reader.read();
+                if (data == -1) break;
+                System.out.println((char) data);
+            }
+
+            System.out.println("===================================");
+
+            // 문자 배열로 읽기
+            reader = new FileReader("C:/Temp/test.txt");
+            char[] data = new char[100]; // 100개의 문자열을 읽어온다.
+
+            while (true) {
+                int num = reader.read(data);
+                if (num == -1) break;
+                for (int i = 0; i < num; i++) {
+                    System.out.println(data[i]);
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
